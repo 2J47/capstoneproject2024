c79a414db6b01f089e69611e911f230aa2200915
2J47 <2jag47@gmail.com>
2025-12-10 15:44:45 +0900

    first commit

---
+++ server/backend/db.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('DB 연결 실패:', err.stack);
+        return;
+    }
+    console.log('DB 연결 성공');
+});
+
+module.exports = connection;
